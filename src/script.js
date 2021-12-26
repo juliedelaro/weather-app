678cbbe52266a1aa51912d09cfe9e627d63d2760
juliedelaro <93611091+juliedelaro@users.noreply.github.com>
2021-12-26 13:23:43 -0500

    removed redundant function and console logs

--- src/script.js
+++ src/script.js
@@ -50,7 +50,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "80eaf0f2f7c0ac14a85fb2a01d28b5a7";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayForecast);
@@ -91,27 +90,12 @@ function handleSubmit(event) {
   search(input.value);
 }
 
-function showCurrentWeather(response) {
-  document.querySelector(".citySearch").innerHTML = response.data.name;
-  document.querySelector("#current-temp").innerHTML = Math.round(
-    response.data.main.temp
-  );
-  document.querySelector("#current-humidty").innerHTML =
-    response.data.main.humidity;
-
-  document.querySelector("#wind-speed").innerHTML = Math.round(
-    response.data.wind.speed
-  );
-  document.querySelector("#description").innerHTML =
-    response.data.weather[0].description;
-}
-
 function getCurrentLocation(position) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
-  axios.get(url).then(showCurrentWeather);
+  axios.get(url).then(displayWeather);
 }
 
 function retrievePosition(event) {
